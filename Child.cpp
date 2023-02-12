64c6a29238da1e27a3fff5846adaccd0f2cc2aca
Kuchma <desferto@mail.ru>
2023-02-12 16:06:49 +0500

    lv

--- Child.cpp
+++ Child.cpp
@@ -3,7 +3,7 @@
 Child::Child() : _name("Billy"), _age(0){}
 
 void  Child::PrintName() {
-	std::cout << "Child name"<< _name << std::endl;
+	std::cout << "Child name "<< _name << std::endl;
 };
 
 int  Child::getAge(int age) {
--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <memory>
 #include "Parent.h"
 #include "Child.h"
 
 int main() {
-
-
-
+	
+	std::shared_ptr<Parent> ch = std::make_shared<Child>();
+	ch->PrintName();
+	auto pch = std::dynamic_pointer_cast <Child>(ch);
+	if (!pch) {
+		std::cout << " pch -> nullptr" << std::endl;
+	}
+	else
+	pch->PrintName();
+	
 	return 0;
 }
